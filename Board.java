4712ac2e7301cff6e5ba8c48b6aa50506d846d57
Praven <praven2010@live.com>
2024-08-12 20:58:51 -0500

    Initial Commit

---
+++ Board.java
@@ -0,0 +1,44 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Board {
+
+    public Board(int[][] tiles) {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public int dimension() {
+
+    }
+
+    public int hamming() {
+
+    }
+
+    public int mahattan() {
+
+    }
+
+    public boolean isGoal() {
+
+    }
+
+    public boolean equals(Object y) {
+
+    }
+
+    public Iterable<Board> neighbors() {
+
+    }
+
+    public Board twin() {
+
+    }
+}
---
+++ Solver.java
@@ -0,0 +1,24 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+
+    public Solver(Board initail) {
+
+    }
+
+    public boolean isSolvable() {
+
+    }
+
+    public int moves() {
+
+    }
+
+    public Iterable<Board> solution() {
+
+    }
+}
