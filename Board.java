7b74748f33c59c3bcc46ddbcdc79515b2a420ae5
Praven <praven2010@live.com>
2024-08-14 17:31:07 -0500

    Update

--- Board.java
+++ Board.java
@@ -4,41 +4,202 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+
 public class Board {
+
+    private int[][] tiles;
+
+    private int n;
+
+    private int blankRow;
+
+    private int blankCol;
+
 
     public Board(int[][] tiles) {
 
+        if (tiles == null) {
+            throw new IllegalArgumentException();
+        }
+
+        n = tiles.length;
+
+        this.tiles = new int[n][n];
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                this.tiles[i][j] = tiles[i][j];
+
+                if (tiles[i][j] == 0) {
+                    blankRow = i;
+                    blankCol = j;
+                }
+            }
+        }
     }
 
     public String toString() {
 
+        StringBuilder gridString = new StringBuilder();
+
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                gridString.append(tiles[i][j]);
+                gridString.append("\t");
+            }
+            gridString.append("\n");
+        }
+
+        return tiles.length + "\n" + gridString;
     }
 
+    //
     public int dimension() {
-
+        return tiles.length;
     }
 
     public int hamming() {
 
+        int distance = 0;
+
+        for (int i = 0; i < n * n; i++) {
+
+            int row = i / n;
+            int col = i % n;
+
+            if (tiles[row][col] != 0 && tiles[row][col] != i + 1) {
+                distance++;
+            }
+        }
+
+        return distance;
     }
 
-    public int mahattan() {
+    public int manhattan() {
 
+        int distance = 0;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+
+                int value = tiles[row][col];
+
+                if (value != 0) {
+                    int targetRow = (value - 1) / n;
+                    int targetCol = (value - 1) % n;
+
+                    distance += Math.abs(row - targetRow) + Math.abs(col - targetCol);
+                }
+            }
+        }
+
+        return distance;
     }
+
 
     public boolean isGoal() {
-
+        return manhattan() == 0;
     }
+
 
     public boolean equals(Object y) {
 
+        if (this == y) return true;
+
+        if (y == null || y.getClass() != getClass()) {
+            return false;
+        }
+
+        Board that = (Board) y;
+
+        if (dimension() != that.dimension()) {
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] != that.tiles[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public Iterable<Board> neighbors() {
 
+        ArrayList<Board> neighbors = new ArrayList<>();
+
+        int[][] directions = {
+                { 0, -1 },
+                { 0, 1 },
+                { -1, 0 },
+                { 1, 0 },
+                };
+
+        for (int[] direction : directions) {
+
+            int row = blankRow + direction[0];
+            int col = blankCol + direction[1];
+
+            if (row >= 0 && row < n && col >= 0 && col < n) {
+                int[][] newNeighborTiles = copyTiles();
+
+                newNeighborTiles[blankRow][blankCol] = tiles[row][col];
+                newNeighborTiles[row][col] = 0;
+                neighbors.add(new Board(newNeighborTiles));
+            }
+        }
+
+        return neighbors;
+    }
+
+    private int[][] copyTiles() {
+
+        int[][] copy = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = tiles[i][j];
+            }
+        }
+
+        return copy;
     }
 
     public Board twin() {
 
+        int[][] newTiles = copyTiles();
+        int row = (tiles[0][0] == 0 || tiles[0][1] == 0) ? 1 : 0;
+        int temp = newTiles[row][0];
+        newTiles[row][0] = newTiles[row][1];
+        newTiles[row][1] = temp;
+        return new Board(newTiles);
+    }
+
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+        StdOut.println(initial);
+
+        for (Board board : initial.neighbors()) {
+            StdOut.println(board);
+        }
+
+        System.out.println("Twin board:");
+        System.out.println(initial.twin());
     }
 }
--- Solver.java
+++ Solver.java
@@ -4,21 +4,149 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Solver {
 
-    public Solver(Board initail) {
+    private Board initial;
 
+    private Stack<Board> path;
+
+    private boolean solvable;
+
+    public Solver(Board initial) {
+
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.initial = initial;
+        path = new Stack<>();
+        solvable = false;
+
+        findSolution();
+    }
+
+    private void findSolution() {
+        MinPQ<SearchNode> queue = new MinPQ<>();
+
+        queue.insert(new SearchNode(initial, null, false));
+        queue.insert(new SearchNode(initial.twin(), null, true));
+
+        while (!queue.isEmpty()) {
+
+            SearchNode node = queue.delMin();
+
+            if (node.board.isGoal()) {
+                if (node.isTwin) {
+                    solvable = false;
+                }
+                else {
+                    solvable = true;
+                    reconstructurePath(node);
+                }
+
+                break;
+            }
+
+            for (Board neighbor : node.board.neighbors()) {
+                if (node.previous == null || !node.previous.board.equals(neighbor)) {
+                    queue.insert(new SearchNode(neighbor, node, node.isTwin));
+                }
+            }
+        }
+    }
+
+    private void reconstructurePath(SearchNode starting) {
+        // Traverse the path
+        SearchNode pointer = starting;
+        while (pointer != null) {
+            path.push(pointer.board);
+            pointer = pointer.previous;
+        }
     }
 
     public boolean isSolvable() {
 
+        return solvable;
     }
 
     public int moves() {
 
+        if (isSolvable()) {
+            return path.size() - 1;
+        }
+
+        return -1;
     }
 
     public Iterable<Board> solution() {
 
+        if (isSolvable()) {
+            return path;
+        }
+
+        return null;
+    }
+
+    private class SearchNode implements Comparable<SearchNode> {
+
+        private Board board;
+
+        private int moves;
+
+        private int manhattan;
+
+        private int priority;
+
+        private boolean isTwin;
+
+        private SearchNode previous;
+
+        public SearchNode(Board board, SearchNode previous, boolean isTwin) {
+            this.board = board;
+            this.previous = previous;
+            this.isTwin = isTwin;
+            this.moves = previous == null ? 0 : previous.moves + 1;
+            this.manhattan = board.manhattan();
+            this.priority = moves + manhattan;
+        }
+
+        @Override
+        public int compareTo(SearchNode that) {
+            if (this.priority != that.priority) {
+                return this.priority - that.priority;
+            }
+            // Use the Manhattan distance for tie-breaking
+            return this.manhattan - that.manhattan;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution()) {
+                StdOut.println(board);
+            }
+        }
     }
 }
